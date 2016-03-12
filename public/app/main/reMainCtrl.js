d51de8a029abaf95f2157c539c06fb0a1e223be3
James <jamesjaywoo@gmail.com>
2016-03-12 15:06:03 -0800

    change controller

--- public/app/main/reMainCtrl.js
+++ public/app/main/reMainCtrl.js
@@ -3,32 +3,37 @@ angular.module('app').controller('reMainCtrl', function($scope, $window) {
         { location: 'Shelbourne Corridor', time: 'January' }
     ]
 
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-
-    function success(pos) {
-        var crd = pos.coords;
-
-        console.log('Your current position is:');
-        console.log('Latitude : ' + crd.latitude);
-        console.log('Longitude: ' + crd.longitude);
-        console.log('More or less ' + crd.accuracy + ' meters.');
-    };
-
-    function error(err) {
-        console.warn('ERROR(' + err.code + '): ' + err.message);
-    };
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
     $( document ).ready(function() {
-        L.map('map', {
-            layers: MQ.mapLayer(),
-            center: [ 40.731701, -73.993411 ],
-            zoom: 12
-        });
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+
+        function success(pos) {
+            var crd = pos.coords;
+
+            console.log('Your current position is:');
+            console.log('Latitude : ' + crd.latitude);
+            console.log('Longitude: ' + crd.longitude);
+            console.log('More or less ' + crd.accuracy + ' meters.');
+
+            L.map('map', {
+                layers: MQ.mapLayer(),
+                center: [ crd.latitude, crd.longitude ],
+                zoom: 12
+            });
+        };
+
+        function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+            L.map('map', {
+                layers: MQ.mapLayer(),
+                center: [51.505, -0.09],
+                zoom: 12
+            });
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
     });
 });
