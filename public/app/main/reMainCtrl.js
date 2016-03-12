b120f9802e15c3e4572d478bc94d920a4dec39db
James <jamesjaywoo@gmail.com>
2016-03-12 14:32:38 -0800

    added map

--- public/app/main/reMainCtrl.js
+++ public/app/main/reMainCtrl.js
@@ -1,5 +1,34 @@
-angular.module('app').controller('reMainCtrl', function($scope) {
+angular.module('app').controller('reMainCtrl', function($scope, $window) {
     $scope.recentBreakIns = [
         { location: 'Shelbourne Corridor', time: 'January' }
     ]
+
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log('Latitude : ' + crd.latitude);
+        console.log('Longitude: ' + crd.longitude);
+        console.log('More or less ' + crd.accuracy + ' meters.');
+    };
+
+    function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+    $( document ).ready(function() {
+        L.map('map', {
+            layers: MQ.mapLayer(),
+            center: [ 40.731701, -73.993411 ],
+            zoom: 12
+        });
+    });
 });
